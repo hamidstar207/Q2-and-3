317c2c64af770c4633a2b9cd8086483ab5dde1fd
hamidstar207 <162834853+hamidstar207@users.noreply.github.com>
2024-03-26 00:09:03 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+// lower case
+let personName = "Hamid";
+console.log("lowercase:", personName.toLowerCase());
+// upper case
+console.log("uppercase:", personName.toLocaleUpperCase());
+// title case
+console.log("titlecase:", personName);
---
+++ main.ts
@@ -0,0 +1,9 @@
+// lower case
+let personName: string = "Hamid"
+console.log("lowercase:", personName.toLowerCase());
+
+// upper case
+console.log("uppercase:", personName.toLocaleUpperCase());
+
+// title case
+console.log("titlecase:", personName)
